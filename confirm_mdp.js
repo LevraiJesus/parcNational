9bf4388b5e9113b28fffe1e31584ca1d6da1993b
rayan-ahamadi <rayan.ahamadi@laplateforme.io>
2024-09-19 11:25:56 +0200

    créer la requête fetch pour la création de users

--- confirm_mdp.js
+++ confirm_mdp.js
@@ -11,3 +11,46 @@ document.querySelector('#confirm-password').addEventListener('blur', function() 
 })
 
 
+document.querySelector('#password').addEventListener('input', function() {
+  if(this.value.length >= 8) {
+    changeTextColor(document.querySelector('#eight-characters'),true);
+  } else {
+    changeTextColor(document.querySelector('#eight-characters'),false);
+  }
+
+  if(this.value.match(/[A-Z]/)) {
+    changeTextColor(document.querySelector('#uppercase-letter'),true);
+  } else {
+    changeTextColor(document.querySelector('#uppercase-letter'),false);
+  }
+
+  if(this.value.match(/[a-z]/)) {
+    changeTextColor(document.querySelector('#lowercase-letter'),true);
+  } else {
+    changeTextColor(document.querySelector('#lowercase-letter'),false);
+  }
+
+  if(this.value.match(/[0-9]/)) {
+    changeTextColor(document.querySelector('#number'),true);
+  } else {
+    changeTextColor(document.querySelector('#number'),false);
+  }
+
+  if(this.value.match(/[^A-Za-z0-9]/)) {
+    changeTextColor(document.querySelector('#special-character'),true);
+  } else {
+    changeTextColor(document.querySelector('#special-character'),false);
+  }
+
+  
+});
+
+function changeTextColor(element,valid){
+  if(valid){
+    element.classList.remove('text-danger');
+    element.classList.add('text-success');
+  } else {
+    element.classList.add('text-danger');
+    element.classList.remove('text-success');
+  }
+}
---
+++ create_user.js
@@ -0,0 +1,28 @@
+document.querySelector('#signup-form').addEventListener('submit', function(e) {
+  e.preventDefault();
+
+  const formData = new FormData(this);
+  let formObject = {};
+  formData.forEach(function(value, key) {
+    formObject[key] = value;
+  });
+
+  const json = JSON.stringify(formObject);
+
+  
+
+  fetch('./users', {
+    method: 'POST',
+    body: json,
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
+  .then(response => response.json())
+  .then(data => {
+    console.log('Success:', data);
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
+});
