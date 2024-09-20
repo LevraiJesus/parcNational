3d30dde8f846b70a9cac66c7b73c75717b93cfbf
alyxandre-beniahia <alyxandre.beniahia@laplateforme.io>
2024-09-20 17:17:58 +0200

    added longitude and latitude for trails

--- backend/models/trail.php
+++ backend/models/trail.php
@@ -7,8 +7,10 @@ class trail {
 
     public $id;
     public $name;
-    public $posStart;
-    public $posEnd;
+    public $longitudeStart;
+    public $longitudeEnd;
+    public $latitudeStart;
+    public $latitudeEnd;
     public $distance;
     public $heightDiff;
     public $pointOfInterest;
@@ -22,7 +24,7 @@ class trail {
     public function create() {
         error_log("Trail model: create method called");
         $query = "INSERT INTO " . $this->table_name . " 
-                  SET name=:name, posStart=:posStart, posEnd=:posEnd, distance=:distance, 
+                  SET name=:name, longitudeStart=:longitudeStart, longitudeEnd=:longitudeEnd, latidudeStart=:latitudeStart, latitudeEnd=:latitudeEnd, distance=:distance, 
                       heightDiff=:heightDiff, pointOfInterest=:pointOfInterest, camping=:camping, difficulty=:difficulty, 
                       created_at=NOW(), modified_at=NOW()";
 
@@ -32,8 +34,10 @@ class trail {
         $this->sanitize();
 
         $stmt->bindParam(":name", $this->name);
-        $stmt->bindParam(":posStart", $this->posStart);
-        $stmt->bindParam(":posEnd", $this->posEnd);
+        $stmt->bindParam(":longitudeStart", $this->longitudeStart);
+        $stmt->bindParam(":longitudeEnd", $this->longitudeEnd);
+        $stmt->bindParam(":latitudeStart", $this->latitudeStart);
+        $stmt->bindParam(":latitudeEnd", $this->latitudeEnd);
         $stmt->bindParam(":distance", $this->distance);
         $stmt->bindParam(":heightDiff", $this->heightDiff);
         $stmt->bindParam(":pointOfInterest", $this->pointOfInterest);
@@ -41,7 +45,7 @@ class trail {
         $stmt->bindParam(":difficulty", $this->difficulty);
     
 
-    error_log("Bound parameters: " . print_r([$this->name, $this->posStart, $this->posEnd, $this->distance, $this->heightDiff, $this->pointOfInterest, $this->camping, $this->difficulty], true));
+    error_log("Bound parameters: " . print_r([$this->name, $this->longitudeStart, $this->latitudeStart, $this->longitudeEnd, $this->latitudeEnd, $this->distance, $this->heightDiff, $this->pointOfInterest, $this->camping, $this->difficulty], true));
 
     if ($stmt->execute()) {
         error_log("Trail model: Trail created");
@@ -63,8 +67,10 @@ class trail {
         if($row) {
             $this->id = $row['id'];
             $this->name = $row['name'];
-            $this->posStart = $row['posStart'];
-            $this->posEnd = $row['posEnd'];
+            $this->longitudeStart = $row['longitudeStart'];
+            $this->longitudeEnd = $row['longitudeEnd'];
+            $this->latitudeStart = $row['latitudeStart'];
+            $this->latitudeEnd = $row['latitudeEnd'];
             $this->distance = $row['distance'];
             $this->heightDiff = $row['heightDiff'];
             $this->pointOfInterest = $row['pointOfInterest'];
@@ -78,7 +84,7 @@ class trail {
 
     public function update() {
         $query = "UPDATE " . $this->table_name . " 
-                  SET name=:name, posStart=:posStart, posEnd=:posEnd, distance=:distance, 
+                  SET name=:name, longitudeStart=:longitudeStart, longitudeEnd=:longitudeEnd, latidudeStart=:latitudeStart, latitudeEnd=:latitudeEnd, distance=:distance, 
                       heightDiff=:heightDiff, pointOfInterest=:pointOfInterest, camping=:camping, difficulty=:difficulty, 
                       modified_at=NOW() 
                   WHERE id=:id";
@@ -88,8 +94,10 @@ class trail {
         $this->sanitize();
 
         $stmt->bindParam(":name", $this->name);
-        $stmt->bindParam(":posStart", $this->posStart);
-        $stmt->bindParam(":posEnd", $this->posEnd);
+        $stmt->bindParam(":longitudeStart", $this->longitudeStart);
+        $stmt->bindParam(":longitudeEnd", $this->longitudeEnd);
+        $stmt->bindParam(":latitudeStart", $this->latitudeStart);
+        $stmt->bindParam(":latitudeEnd", $this->latitudeEnd);
         $stmt->bindParam(":distance", $this->distance);
         $stmt->bindParam(":heightDiff", $this->heightDiff);
         $stmt->bindParam(":pointOfInterest", $this->pointOfInterest);
@@ -121,8 +129,10 @@ class trail {
 
     private function sanitize() {
         $this->name = htmlspecialchars(strip_tags($this->name));
-        $this->posStart = htmlspecialchars(strip_tags($this->posStart));
-        $this->posEnd = htmlspecialchars(strip_tags($this->posEnd));
+        $this->longitudeStart = htmlspecialchars(strip_tags($this->longitudeStart));
+        $this->longitudeEnd = htmlspecialchars(strip_tags($this->longitudeEnd));
+        $this->latitudeStart = htmlspecialchars(strip_tags($this->latitudeStart));
+        $this->latitudeEnd = htmlspecialchars(strip_tags($this->latitudeEnd));
         $this->distance = htmlspecialchars(strip_tags($this->distance));
         $this->heightDiff = htmlspecialchars(strip_tags($this->heightDiff));
         $this->pointOfInterest = htmlspecialchars(strip_tags($this->pointOfInterest));
